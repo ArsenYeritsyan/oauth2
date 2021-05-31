b47fbca0befb96dc68eb89546ed0f74a7b6ef58f
ArsenYeritsyan <yeritsyan.arsen@gmail.com>
2021-05-31 16:42:25 +0400

    Oauth2

---
+++ src/main/java/com/example/todooauth2/ToDoRepository.java
@@ -0,0 +1,8 @@
+package com.example.todooauth2;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ToDoRepository extends CrudRepository<ToDo,String> {
+}
